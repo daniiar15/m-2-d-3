de19207ff100afbbe5f12f19d98c601e492cd421
daniiar15 <daniiar.elebesov.kk.com>
2024-03-23 15:38:55 +0600

    gotov

--- main.py
+++ main.py
@@ -25,46 +25,64 @@
 '''
 2) Класс студент
 '''
-# class Student:
-#     def __init__(self, name) -> None:
-#         self.name = name
-#         self.grate = None
+class Student:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.grade = None
 
-#     def set_grade(self, grade):
-#         self.grate = grade
+    def set_grade(self, grade):
+        self.grade = grade
 
-#     def get_grade(self):
-#         if self.grate is None:
-#             print(f'У студента не проставлена оценка!')
-#         else:
-#             print('Оценка студента: {self.grade}')
+    def get_grade(self):
+        if self.grade is None:
+            print(f'У студента не проставлена оценка!')
+        else:
+            print('Оценка студента: {self.grade}')
+     
 
 
-# student1 = Student('Daniiar')
-# student1.get_grade()
-# student1.set_grade(83)    
-# student1.get_grade()
+student1 = Student('Daniiar')
+student1.get_grade()
+student1.set_grade(83)    
+student1.get_grade()
+
 
 '''
 3) Наследование - Класс Преподаватель
 '''
 
-class Teacher:
-    def __init__(self, name, subjekct):
-        self.name = name
-        self.subject = subjekct
+# class Teacher:
+#     def __init__(self, name, subjekct):
+#         self.name = name
+#         self.subject = subjekct
 
-    def info(self):
-        print(f'{self.name}Предмет преподавания {self.subject}')
+#     def info(self):
+#         print(f'{self.name}Предмет преподавания {self.subject}')
 
-class TeacherMath(Teacher):
-    pass
+# class TeacherMath(Teacher):
+#     pass
 
-class TeacherLang(Teacher):
-    pass
+# class TeacherLang(Teacher):
+#     pass
 
-teachr_math = TeacherMath(name='Фибоначчи', subjekct='Математик')
-teachr_lang = TeacherLang(name='Гвидо Ван Россум', subjekct='python')
+# teachr_math = TeacherMath(name='Фибоначчи', subjekct='Математик')
+# teachr_lang = TeacherLang(name='Гвидо Ван Россум', subjekct='python')
 
-teachr_math.info()
-teachr_lang.info()
+# teachr_math.info()
+# teachr_lang.info()
+
+'''
+4) Класс Транспортное Средство
+'''
+
+# class Vechicle:
+#     def __init__(self, mark, model):
+#         self.mark = mark
+#         self.model = model
+
+#     def start_engine(self):
+#         print(f'{self.mark}-{self.model} заводись')
+              
+# car1 = Vechicle(mark='Toyota', model='Camry')
+# car1.start_engine()
+
